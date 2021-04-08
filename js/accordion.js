9f8195f9e8968b845b6147f71f93704db9c532a4
Tomáš <tomas.kubicek@doctrina.cz>
2021-04-08 15:53:16 +0200

    Oprava .htaccess a přidání šipek v pravidlech pomocí fontawesome

--- js/accordion.js
+++ js/accordion.js
@@ -2,6 +2,8 @@ var h2s = document.querySelectorAll("h2");
 h2s.forEach(h2 => {
     h2.addEventListener("click", (e) => {
         e.target.classList.toggle("active");
+        e.target.querySelector("i").classList.toggle("fa-chevron-right")
+        e.target.querySelector("i").classList.toggle("fa-chevron-down")
 
         var panel = e.target.nextElementSibling;
         if (panel.style.display === "block") {
