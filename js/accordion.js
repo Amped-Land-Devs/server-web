94042ec77c58838ff186e87f252632a9c9a6e5e2
Tomáš <tomas.kubicek@doctrina.cz>
2021-04-08 16:00:43 +0200

    Transition u šipky

--- js/accordion.js
+++ js/accordion.js
@@ -2,14 +2,14 @@ var h2s = document.querySelectorAll("h2");
 h2s.forEach(h2 => {
     h2.addEventListener("click", (e) => {
         e.target.classList.toggle("active");
-        e.target.querySelector("i").classList.toggle("fa-chevron-right")
-        e.target.querySelector("i").classList.toggle("fa-chevron-down")
 
         var panel = e.target.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
+            e.target.querySelector("i").style.transform = "rotate(0deg)"
         } else {
             panel.style.display = "block";
+            e.target.querySelector("i").style.transform = "rotate(90deg)"
         }
     });
 });
